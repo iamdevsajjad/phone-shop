7b420cad0039397adc24faa889edff76fcd89ad8
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-11-16 07:37:39 +0600

    nav link style added sucessfully

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,17 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className=" shadow-lg py-4 px-2">
       <div className="navBar container mx-auto flex justify-between">
         <div className="logo">
-          <h3 className="text-xl font-semibold"><Link to={"/"}>Phone Shop</Link></h3>
+          <h3 className="text-xl font-semibold">
+            <Link to={"/"}>Phone Shop</Link>
+          </h3>
         </div>
         <div className="nav flex gap-3">
-          <Link to={"/"}>Home</Link>
-          <Link to={"/favorites"}>Favorites</Link>
-          <Link to={"/login"}>Login</Link>
+          <NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "text-green-600 underline" : ""
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/favorites"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "text-green-600 underline" : ""
+            }
+          >
+            Favorites
+          </NavLink>
+          <NavLink
+            to="/login"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "text-green-600 underline" : ""
+            }
+          >
+            Login
+          </NavLink>
         </div>
       </div>
     </header>
