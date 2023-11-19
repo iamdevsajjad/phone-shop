260c72309cd82549f08239f43048ff6e18e6cf7f
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-11-19 22:51:06 +0600

    favorite route added and set sucessfully

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Favorite = () => {
+    const [favorites, setFavorites] = useState([]);
+    const [noFound, setNoFound] = useState("")
+    useEffect( ()=>{
+const favoriteItems = JSON.parse(localStorage.getItem("favorites"))
+if(favoriteItems){
+    setFavorites(favoriteItems);
+}else{
+    setNoFound("NO Data Found")
+}
+
+    },[])
+console.log(favorites);
+
+
+    
     return (
         <div>
             favorite route here
+            {noFound}
         </div>
     );
 };
